69663a6d8189d3f95ffa0a86725b23f7cb1a2a99
Yuval Shavit <yankee.sierra@gmail.com>
2023-08-08 04:56:13 -0400

    checkpoint: using rocket

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,57 @@
 use aws_lambda_events::lambda_function_urls::LambdaFunctionUrlRequest;
 use lambda_runtime::{service_fn, Error, LambdaEvent};
 use serde_json::{json, Value};
-use std::collections::HashMap;
+use std::str::FromStr;
+use rocket::http::Method;
+use rocket::local::asynchronous::Client;
 
-async fn handler(event: LambdaEvent<LambdaFunctionUrlRequest>) -> Result<Value, Error> {
+#[macro_use] extern crate rocket;
+
+async fn handler(client: &Client, event: LambdaEvent<LambdaFunctionUrlRequest>) -> Result<Value, Error> {
+    let Some(method) = event.payload.request_context.http.method else {
+        return Err("couldn't determine HTTP method".into());
+    };
+    let Ok(method) = Method::from_str(&method) else {
+        return Err("couldn't determine HTTP method".into());
+    };
+    let Some(path) = &event.payload.raw_path else {
+        return Err("couldn't determine path".into());
+    };
+    let builder = match method {
+        Method::Get => Client::get,
+        Method::Put => Client::put,
+        Method::Post => Client::post,
+        Method::Delete => Client::delete,
+        Method::Options => Client::options,
+        Method::Head => Client::head,
+        Method::Patch => Client::patch,
+        _ => panic!("TODO")
+    };
+    let result = builder(&client, path).dispatch().await;
+    let result_status = (&result.status()).to_string();
+    let result_body = result.into_string().await.unwrap();
+
+
     let path = &event.payload.raw_path;
-    let headers = &event.payload.headers;
-    let mut headers_map = HashMap::with_capacity(headers.len());
-    for (k, v) in headers {
-        if let Ok(val) = v.to_str() {
-            headers_map.insert(k.as_str(), val);
-        }
-    }
     Ok(json!({
         "message": format!("Hello, world!"),
-        "headers-echo": headers_map,
+        "method": method,
         "path": path,
+        "result":     result_status,
+        "result_msg": result_body,
     }))
+}
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world! It's me!"
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    lambda_runtime::run(service_fn(handler)).await
+    let rocket = rocket::build().mount("/", routes![hello]);
+    let client = Client::untracked(rocket).await?;
+    lambda_runtime::run(service_fn(|event| {
+        handler(&client, event)
+    })).await
 }
